0194ff61e3be348e504aca87b671832f99e3e810
saikumarpd22 <saikumarpd22@gmail.com>
2024-04-03 18:04:41 +0530

    initiation of second jdbc

---
+++ src/jdbc_project2/Hello.java
@@ -0,0 +1,12 @@
+package jdbc_project2;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello this is jdbc 2 repository");
+		
+	}
+
+}
